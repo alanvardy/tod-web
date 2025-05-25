72a6dfa2e7e55cb579d9a0dbd630a28e1ca58396
Alan Vardy <alanvardy@gmail.com>
2025-05-24 18:31:53 -0700

    Improve UI and reorganize scripts

---
+++ templates/base.js
@@ -0,0 +1,7 @@
+$(".navbar-burger").click(function() {
+  $(".navbar-menu").addClass("is-active");  
+});
+
+$(".navbar-burger").click(function() {
+   $(".navbar-menu").removeClass("is-active");
+});
